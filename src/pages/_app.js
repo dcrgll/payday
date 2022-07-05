d16a22cfdea478c9e88640f084f5870e00dbaa92
dan <8881965+dcrgll@users.noreply.github.com>
2022-07-05 12:58:22 +0100

    Update Eslint Add Eslint-plugin

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,10 @@
+import PropTypes from 'prop-types'
+
 export default function Payday({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+Payday.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired
 }
--- tests/components/home_page_content/index.test.js
+++ tests/components/home_page_content/index.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import HomePageContent from '@/components/home_page_content'
 
 it('renders a heading', () => {
-  const { container } = render(<HomePageContent />)
+  render(<HomePageContent />)
 
   const heading = screen.getByRole('heading', {
     name: 'hello'
