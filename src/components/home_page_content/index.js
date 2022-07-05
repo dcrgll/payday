12d5bb95a2d8733d49145a44827fca1b80e20f5c
dan <8881965+dcrgll@users.noreply.github.com>
2022-07-05 14:23:18 +0100

    Update Firebase

--- src/components/home_page_content/index.js
+++ src/components/home_page_content/index.js
@@ -1,9 +1,15 @@
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { auth } from 'config/firebase'
 import * as styles from './styles'
 
 export default function HomePageContent() {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth)
+
+  console.log('Loading:', loading, '|', 'Current user:', user)
   return (
     <div css={styles.container}>
       <h1>hello</h1>
+      <button onClick={() => signInWithGoogle()}>hello</button>
     </div>
   )
 }
